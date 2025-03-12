643c42e8366c70383a91b9fa61befca95cfa6a6b
Ferdi <57798860+ImHyper01@users.noreply.github.com>
2025-03-12 10:35:30 +0100

    kleine layout gelegd

---
+++ src/app/pages/agenda/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./style.module.scss";
+
+export default function Index() {
+    return (
+        <div>
+            <p>hello</p>
+        </div>
+    )
+}
