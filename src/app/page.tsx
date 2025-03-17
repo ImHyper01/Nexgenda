e6073a71bd75403fb6a284d6b9dd9ddef7366962
Ferdi <57798860+ImHyper01@users.noreply.github.com>
2025-03-17 12:41:32 +0100

    sign in/sign out forms toegevoegd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import Header from '../components/header';
 
 export default function index() {
 
   return (
-    <div>
-      <p>hello</p>
-    </div>
+   <Header />
   )
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,26 @@
+'use client';
+import React from "react";
+import { useRouter } from "next/navigation";
+import style from './style.module.scss';
+
+export default function Index() {
+    const router = useRouter();
+
+    return (
+        <div className={style.header}> 
+            <div className={style.section1}>
+                <div className={style.logo}>
+                    <img src="./VisionFlowLogo.png" alt="logo" />
+                </div>
+            </div>
+
+            <div className={style.section2}>
+                <div className={style.button1}>
+                    <button className={style.signInButton} onClick={() => router.push('/signIn')}>
+                        Sign In
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
